180fce6b57ef834134db33f4a39db6908a9d91ae
Dennis_Slobodzian <de.slobodzian@gmail.com>
2026-05-13 05:21:27 -0400

    wip

--- tools/subsystem_codegen/parser.py
+++ tools/subsystem_codegen/parser.py
@@ -1,36 +1,31 @@
 import tomllib
 from pathlib import Path
 from model import Subsystem, Motor, MotorControllerType
+from collections import defaultdict
+
+VALID_SUBSYSTEM_KEYS = set([
+    "subsystem",
+    "motors",
+    "sensors",
+])
 
 
-def verify_toml_subsystem(subsystem_data: dict):
-    # Verify keys [subsystem, motors, sensors]
-    valid_keys = set(["subsystem", "motors", "sensors"])
-    print(subsystem_data.keys())
-    key_subset = set(subsystem_data.keys())
+def find_duplicate_motors(motors: list[Motor]):
+    grouped = defaultdict(list)
+    for motor in motors:
+        grouped[(motor.canbus, motor.can_id)].append(motor)
 
-    subset_diff = key_subset.difference(valid_keys)
-    if not key_subset.issubset(valid_keys):
-        print(f"Subsystem toml keys not in valid keys: {subset_diff}")
-        return False
-
-    return True
+    return {
+        key: group
+        for key, group in grouped.items()
+        if len(group) > 1
+    }
 
 
-# def verify_motor(motors: list[Motor]):
-def parse_subsystem(toml_path: Path):
-    with open(toml_path, "rb") as f:
-        subsystem_data = tomllib.load(f)
-
-    if not verify_toml_subsystem(subsystem_data):
-        return None
-
-    subsystem = subsystem_data["subsystem"]
-
+def extract_motor_data(subsystem_data) -> list[Motor]:
     motors = []
     for motor in subsystem_data["motors"]:
         motor_data = subsystem_data["motors"][motor]
-        print(motor)
         motors.append(
             Motor(
                 type=MotorControllerType(motor_data["type"]),
@@ -40,8 +35,37 @@ def parse_subsystem(toml_path: Path):
                 inverted=motor_data["inverted"],
             )
         )
+    return motors
 
-    print(motors)
+
+def verify_toml_subsystem(subsystem_data: dict):
+    print(subsystem_data.keys())
+    key_subset = set(subsystem_data.keys())
+
+    subset_diff = key_subset.difference(VALID_SUBSYSTEM_KEYS)
+    if not key_subset.issubset(VALID_SUBSYSTEM_KEYS):
+        print(f"Subsystem toml keys not in valid keys: {subset_diff}")
+        return False
+
+    motors = extract_motor_data(subsystem_data)
+    duplicate_motors = find_duplicate_motors(motors)
+    for (canbus, id), group in duplicate_motors.items():
+        print(f"Duplicate CAN device: bus={canbus}, id={id}")
+        for motor in group:
+            print(f" - {motor.name}, ({motor.type})")
+
+    return True
+
+
+def parse_subsystem(toml_path: Path):
+    with open(toml_path, "rb") as f:
+        subsystem_data = tomllib.load(f)
+
+    if not verify_toml_subsystem(subsystem_data):
+        return None
+
+    subsystem = subsystem_data["subsystem"]
+
     subsystem = Subsystem(
         name=subsystem["name"],
         subsystem_id=subsystem["id"]
