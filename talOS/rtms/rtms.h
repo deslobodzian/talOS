f9f07c601fade6eb458c064b4e5f3d7fec360ce4
Dennis_Slobodzian <de.slobodzian@gmail.com>
2026-06-26 19:34:19 -0400

    I dont have a good idea yet on structure, but this is somewhat an idea.

--- talOS/rtms/rtms.h
+++ talOS/rtms/rtms.h
@@ -9,37 +9,55 @@
 #include <cstddef>
 #include <atomic>
 #include <cstdint>
+#include <string>
+#include "talOS/memory/shared_memory_ptr.h"
 
 inline constexpr size_t CACHE_LINE = 64; // C++ I think as a function for this.
-static constexpr size_t MAX_BUFFER_SIZE = 1024;
-static constexpr size_t MAX_READERS = 8;
+inline constexpr size_t MAX_BUFFER_SIZE = 1u << 20; // 1MiB
+inline constexpr size_t MASK = MAX_BUFFER_SIZE - 1; // Mask for fixed size
+inline constexpr size_t MAX_READERS = 8;
 
-// Mesages will primarity be using flatbuffers but stored this rtms_msg_t objects
-struct rtms_msg_t {
-    void* data;
+// Mesages will primarity be using flatbuffers but stored this RTMSMessage object
+struct RTMSMessage {
     size_t size;
+    void* data;
 };
 
-enum class reader_status_t : std::uint64_t {
+enum class ReaderStatus : std::uint64_t {
     FREE,
     CLAIMING,
     ACTIVE
 };
 
-struct alignas(CACHE_LINE) writer_t {
+struct alignas(CACHE_LINE) Writer {
     std::atomic<uint64_t> sequence{};
 };
 
-struct alignas(CACHE_LINE) reader_t {
+struct alignas(CACHE_LINE) Reader {
     std::atomic<uint64_t> sequence{};
     std::atomic<uint64_t> slot{};
-    std::atomic<reader_status_t> status{};
+    std::atomic<ReaderStatus> status{};
 };
 
-struct rtms_header_t {
+struct RTMSHeader {
+    // This will be here incase I want to my different sized buffers in the future
+    std::uint64_t capacity;
+    std::size_t message_size; // message size will be fixes for each buffer
+
+    Writer writer;
+    Reader readers[MAX_READERS];
 };
 
 // Ring buffer queue,
-struct rtms_queue_t {
-    //alignas(std::atomic<uint64_t>)
+template <typename T>
+class RTMSQueue {
+public:
+    explicit RTMSQueue(std::string_view path) :
+        path_(path),
+        ptr_(path_.c_str(), 100) {
+    }
+private:
+    std::string path_;
+    RTMSHeader header_;
+    SharedMemoryPtr<void> ptr_;
 };
--- talOS/rtms/test.cc
+++ talOS/rtms/test.cc
@@ -1,7 +1,31 @@
 #include <gtest/gtest.h>
 #include "rtms.h"
+#include <flatbuffers/flatbuffers.h>
+#include "talOS/rtms/test_message_generated.h"
+
+TEST(RTMSMessage, RTMS) {
+    flatbuffers::FlatBufferBuilder builder(1024);
+
+    auto name = builder.CreateString("TestMessage");
+    Message::TestMessageBuilder message_builder(builder);
+    message_builder.add_name(name);
+    message_builder.add_id(12);
+
+    auto message = message_builder.Finish();
+    builder.Finish(message);
+
+    uint8_t* buf = builder.GetBufferPointer();
+    size_t size = builder.GetSize();
+
+
+    std::printf("Message ptr: %p size: %zu", &buf, size);
+
+    [[maybe_unused]]RTMSMessage rtms_message{size, buf};
+
+    EXPECT_EQ(5, 5);
+}
 
 TEST(RTMSTest, RTMS) {
-    std::printf("rtms_queue_t size: %li", sizeof(rtms_queue_t));
+    //std::printf("rtms_queue_t size: %li", sizeof(RTMSQueue));
     EXPECT_EQ(5, 5);
 }
